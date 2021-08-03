c9f1f076b6e5676a745b7ed467625d4a588c8a40
abrusdev <abrusdev@gmail.com>
2021-08-03 13:13:45 +0500

    uodate: some changes with question screen

--- resources/views/home/speaking/question.blade.php
+++ resources/views/home/speaking/question.blade.php
@@ -3,16 +3,23 @@
 @section("content")
 
     <div class="container d-flex justify-content-center flex-column align-items-center p-4">
-        <div class="col-6 bg-white">
+
+        <div class="col-6 w-100 p-0">
+            <form action="{{ route("speaking.topics") }}">
+                <button class="btn btn-dark pl-4 pr-4 mt-4">Back</button>
+            </form>
+        </div>
+
+        <div class="col-6 bg-white mt-4">
             <form action="{{ route("speaking.questions", $id) }}" method="POST" class="p-4">
                 @csrf
                 <div class="mb-3">
                     <h5 class="text-center">{{ $topic }}</h5>
 
                     <label for="name" class="form-label">Questions:</label>
-                    <input type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp" required>
+                    <textarea type="text" name="name" class="form-control" id="name" aria-describedby="emailHelp" required></textarea>
                 </div>
-                <button type="submit" class="btn btn-primary mt-3 w-100 mb-4">Submit</button>
+                <button type="submit" class="btn btn-primary mt-3 w-100 mb-4">Add</button>
             </form>
         </div>
 
@@ -28,7 +35,7 @@
             @foreach($questions as $q)
                 <tr>
                     <th scope="row">{{ $q['id'] }}</th>
-                    <td>{{ $q["name"] }}</td>
+                    <td style="max-width: 100px;word-wrap:break-word;overflow:hidden;">{{ $q["name"] }}</td>
                 </tr>
             @endforeach
             </tbody>
