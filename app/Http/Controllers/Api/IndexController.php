96d01dce444db73ffb30c840051aa166d6269e23
abrusdev <abrusdev@gmail.com>
2021-08-04 11:14:10 +0500

    update: base of api

--- app/Http/Controllers/Api/IndexController.php
+++ app/Http/Controllers/Api/IndexController.php
@@ -15,8 +15,6 @@ class IndexController extends Controller
         $data = [
             "topics" => Topic::all()
         ];
-        Storage::disk('public')->put('all.json', json_encode($data));
-        $path = storage_path(). "/app/public/all.json";
-        return \Response::download($path);
+        return $data;
     }
 }
